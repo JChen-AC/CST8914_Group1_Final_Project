4e0baae2580bc3cb8b35cad81bb189961287d666
JChen-AC <chen1101@algonquinlive.com>
2026-04-10 12:38:57 -0400

    Adding javascript code to swap the page content

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -1,11 +1,29 @@
 //Template Function that can be used to run JavaScript on the page
 //Note: This can be changed to whatever JavaScript formatting you would like
-function knowledgeRunner(){
+function knowledgeRunner() {
+    function clearPage() {
+        const main = document.getElementById("main-content");
+        console.log(main)
+        console.log(main.firstElementChild)
+        // CAN USE THE JQUERY LIKE DEQUE to remove instead 
+        main.removeChild(main.firstElementChild);        
+    }
 
+    function loadPage() {
+        let template = document.getElementById("service-template");
+        let main = document.getElementById("main-content");
+        console.log(template)
+
+        main.appendChild(template.content.cloneNode(true))
+    }
+
+    $(document).on('click', 'a', function (e) {
+        e.preventDefault();
+        clearPage();
+        loadPage();
+        
+    });
 }
-
-
-
 
 
 knowledgeRunner()
