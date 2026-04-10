bc5a88bed4315f30ae1b5842e46eaa01dfc5b745
JChen-AC <chen1101@algonquinlive.com>
2026-04-10 15:25:24 -0400

    Adding functionality to get fixed information about the page and added comments

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -6,7 +6,7 @@ function knowledgeRunner() {
         console.log(main)
         console.log(main.firstElementChild)
         // CAN USE THE JQUERY LIKE DEQUE to remove instead 
-        main.removeChild(main.firstElementChild);        
+        main.removeChild(main.firstElementChild);
     }
 
     function loadPage() {
@@ -17,7 +17,7 @@ function knowledgeRunner() {
 
         main.appendChild(template.content.cloneNode(true))
     }
-    function loadStart(){
+    function loadStart() {
         console.log("Loading main page")
         let start_template = document.getElementById("main-template");
         let main = document.getElementById("main-content");
@@ -29,8 +29,57 @@ function knowledgeRunner() {
         main.appendChild(start_template.content.cloneNode(true))
     }
 
-    $(document).on('click', 'a', function (e) {
+    function updateHistory() {
+        let newURL = "";
+        let newTitle = "";
+        history.pushState({ url: newURL, title: newTitle }, newURL, newTitle);
+    };
+    function updateTitle() {
+
+    };
+    function updateURL() {
+
+    };
+
+    function get_page_detail(e) {
+        console.log(e)
+        if (e.target.text == "Home") {
+            console.log("Link clicked is home")
+            const page_data = {
+                title:"Empower Ability Labs",
+                url:"home",
+                id:"main-template"
+            };
+
+        }
+        else if (e.target.text == "Services") {
+            console.log("link clicked is services")
+            const page_data = {
+                title:"Services - Empower Ability Labs",
+                url:"services",
+                id:"services-template"
+            };
+
+        }
+        else if (e.target.text == "Schedule a call") {
+            console.log("link clicked is Schedule a call")
+            const page_data = {
+                title:"Scehdule a call - Empower Ability Labs",
+                url:"schedule",
+                id:"schedule-template"
+            };
+        }
+    }
+
+    $(document).on('click', 'a.nav-link', function (e) {
+        console.log(e);
+        console.log(e.target.text);
+        console.log(window.location.href)
+        get_page_detail(e);
+
         e.preventDefault();
+        console.log("Getting page details")
+        //console.log(e.target.text);
         clearPage();
         loadPage();
         let heading = document.getElementsByTagName("h1")[0];
@@ -40,5 +89,15 @@ function knowledgeRunner() {
     loadStart();
 }
 
+/*
+What might not be needed
+- add aria live to show that the section has been updated
+- todo
+    - fix it so that it is more general (programatically determine which link is being chosen and load the correct one )
+    - update the history 
+    - be able to go back 
+    - need to change title 
+        - need to change url 
 
+*/
 knowledgeRunner()
