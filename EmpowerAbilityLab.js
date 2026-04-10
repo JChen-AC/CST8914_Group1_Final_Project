b34787185393be5c1f6014380e69e886330ab839
JChen-AC <chen1101@algonquinlive.com>
2026-04-10 12:48:08 -0400

    Moving javascript to the very end and fixing the id for the main template.
    Adding a load start to load the main page content and adding focus when changing pages

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -10,19 +10,35 @@ function knowledgeRunner() {
     }
 
     function loadPage() {
+        console.log("Loading New content")
         let template = document.getElementById("service-template");
         let main = document.getElementById("main-content");
         console.log(template)
 
         main.appendChild(template.content.cloneNode(true))
     }
+    function loadStart(){
+        console.log("Loading main page")
+        let start_template = document.getElementById("main-template");
+        let main = document.getElementById("main-content");
+        console.log("Template: ")
+        console.log(start_template)
+        console.log("main: ")
+        console.log(main)
+
+        main.appendChild(start_template.content.cloneNode(true))
+    }
 
     $(document).on('click', 'a', function (e) {
         e.preventDefault();
         clearPage();
         loadPage();
-        
+        const heading = document.getElementsByTagName("h1")
+        console.log(heading)
+        heading.focus()
     });
+
+    loadStart();
 }
 
 
